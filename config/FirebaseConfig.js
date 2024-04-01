7105c6d66936c727d96941ae16cb808750b8d418
euronymousutsv <utsavkhatri7@gmail.com>
2024-04-01 23:06:49 +1100

    first commit

---
+++ config/FirebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQLO-padWQ86aYlUk85PilOt3pHa-NcWw",
+    authDomain: "crossplatformapplication-57541.firebaseapp.com",
+    projectId: "crossplatformapplication-57541",
+    storageBucket: "crossplatformapplication-57541.appspot.com",
+    messagingSenderId: "37364270073",
+    appId: "1:37364270073:web:1fe966b2b735e46b7bb328"
+  };
